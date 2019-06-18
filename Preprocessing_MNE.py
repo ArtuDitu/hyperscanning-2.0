7534164832650c37f8ec8ef61177c8d690d36665
mtiessen <mtiessen@isonoe.neurobiopsychologie.uni-osnabrueck.de>
2019-06-18 18:05:04 +0200

    changed the requirements file by adding jupyter packages; started the prpcsng script

--- Preprocessing_MNE.py
+++ Preprocessing_MNE.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
-#######################
+############ EEG-Preprocessing Script (Python_MNE toolbox) ##############
+# Credits:
+# Pernet, C. R., Appelhoff, S., Flandin, G., Phillips, C., Delorme, A., &
+# Oostenveld, R. (2018, December 6). BIDS-EEG: an extension to the Brain
+# Imaging Data Structure  (BIDS) Specification for electroencephalography.
+# https://doi.org/10.31234/osf.io/63a4y
+
 # Author: Max Tiessen
 # Mail: mtiessen@uos.de
 # Date: June 2019
-#######################
 
+import os
 import pandas as pd
 import numpy as np
 import mne
@@ -13,4 +19,13 @@ import mne
 from mne_bids import write_raw_bids
 from mne.datasets import sample
 
+
+#############################################
+# STEP ONE: Load data, split into two structs
+#############################################
+os.chdir('/net/store/nbp/projects/hyperscanning/hyperscanning-2.0')
+
+eeglab_montage = '/net/home/student/m/mtiessen/link_hyperscanning/M_tools/eeglab14_1_2b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp'
+mne_montage = mne.channels.read_montage(kind = 'standard_1005') #path = '~/.virtualenvs/hyper-2.0_env/lib/python3.5/site-packages/mne/channels/data/montages'
+raw = mne.io.read_raw_cnt(input_fname = '/home/student/m/mtiessen/link_hyperscanning/EEG_data/sub202/sub202.cnt', montage = mne_montage)
 write_raw_bids(raw, 'sub-01_ses-01_run-05', output_path='./bids_dataset')
